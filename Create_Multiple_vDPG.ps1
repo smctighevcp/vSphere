fda737db35c1d7e915dfc3e6e95b5172fae71d84
smctighevcp <65562694+smctighevcp@users.noreply.github.com>
2020-05-20 18:13:26 +0100

    Create_Multiple_vDPG.ps1

---
+++ Create_Multiple_vDPG.ps1
@@ -0,0 +1,25 @@
+#Load PowerCLI Modules
+Import-module VMware.PowerCLI
+
+###################### Variables - Ammend! #####################
+$vCenter = "<vCenter FQDN or IP>"
+$vDSName = "vDS Switch Name"
+$VDPGS =@(
+            [pscustomobject]@{PG='<PortGroup Name>';VLANID='<VLAN ID>'}#enter 0 for VLANID if no VLAN Tag required
+            [pscustomobject]@{PG='<PortGroup Name>';VLANID='<VLAN ID>'}#enter 0 for VLANID if no VLAN Tag required
+            [pscustomobject]@{PG='<PortGroup Name>';VLANID='<VLAN ID>'}#enter 0 for VLANID if no VLAN Tag required
+            )
+$Ports = "8" #Port allocation is Elastic by default which will increase the port count when the limit is reached by increments of 8
+$LoadBalancing = "LoadBalanceLoadBased" #Based on Physical Nic Load, change as required (LoadBalanceLoadBased, LoadBalanceIP, LoadBalanceSrcMac, LoadBalanceSrcId, ExplicitFailover)
+
+################################################################
+
+#Connect to vCenter
+Connect-VIServer $vCenter -Credential (Get-Credential) -force
+
+#Create Distributed Virtual Port Group.
+ForEach ($VDPG in $VDPGS) 
+                        {Get-VDSwitch -Name $vDSName | New-VDPortGroup -Name $VDPG.PG -VLanId $VDPG.VLANID -NumPorts 8
+                        #Set Load Balancing option
+                        Get-VDswitch -Name $vDSName | Get-VDPortgroup $VDPG.PG | Get-VDUplinkTeamingPolicy | Set-VDUplinkTeamingPolicy -LoadBalancingPolicy $LoadBalancing
+        }
