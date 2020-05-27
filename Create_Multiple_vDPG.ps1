0b82e57c403c21dcdecc294ba300c7de5f49bfea
smctighevcp <65562694+smctighevcp@users.noreply.github.com>
2020-05-27 19:14:09 +0100

    Update Create_Multiple_vDPG.ps1

--- Create_Multiple_vDPG.ps1
+++ Create_Multiple_vDPG.ps1
@@ -11,6 +11,9 @@ $VDPGS =@(
             )
 $Ports = "8" #Port allocation is Elastic by default which will increase the port count when the limit is reached by increments of 8
 $LoadBalancing = "LoadBalanceLoadBased" #Based on Physical Nic Load, change as required (LoadBalanceLoadBased, LoadBalanceIP, LoadBalanceSrcMac, LoadBalanceSrcId, ExplicitFailover)
+$ActiveUP = "Uplink 1" #Modify as required
+$StandUp = "Uplink 2" #Modify as required
+$UnusedUP = "Uplink 3" #Modify as required
 
 ################################################################
 
@@ -21,5 +24,5 @@ Connect-VIServer $vCenter -Credential (Get-Credential) -force
 ForEach ($VDPG in $VDPGS) 
                         {Get-VDSwitch -Name $vDSName | New-VDPortGroup -Name $VDPG.PG -VLanId $VDPG.VLANID -NumPorts 8
                         #Set Load Balancing option
-                        Get-VDswitch -Name $vDSName | Get-VDPortgroup $VDPG.PG | Get-VDUplinkTeamingPolicy | Set-VDUplinkTeamingPolicy -LoadBalancingPolicy $LoadBalancing
+                        Get-VDswitch -Name $vDSName | Get-VDPortgroup $VDPG.PG | Get-VDUplinkTeamingPolicy | Set-VDUplinkTeamingPolicy -LoadBalancingPolicy $LoadBalancing -ActiveUplinkPort $ActiveUP -StandbyUplinkPort $StandUp -UnusedUplinkPort $UnusedUP
         }
