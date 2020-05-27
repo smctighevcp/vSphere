ff29076458ac5fd5688e01f1d2faeed8a23ce006
smctighevcp <65562694+smctighevcp@users.noreply.github.com>
2020-05-27 19:13:35 +0100

    Update Create_Single_vDPG.ps1

--- Create_Single_vDPG.ps1
+++ Create_Single_vDPG.ps1
@@ -2,12 +2,15 @@
 Import-module VMware.PowerCLI
 
 ###################### Variables - Ammend! #####################
-$vCenter = "<vCenter FQDN or IP>"
-$vDSName = "vDS Switch Name"
-$VDPG ="<PortGroup Name>"
-$VLAN = "<VLAN ID>" #enter 0 if no VLAN Tag required
+$vCenter = "vcsa02.lab.local"
+$vDSName = "vDS_Static_Guest"
+$VDPG ="NewPortGroup"
+$VLAN = "0" #enter 0 if no VLAN Tag required
 $Ports = "8" #Port allocation is Elastic by default which will increase the port count when the limit is reached by increments of 8
 $LoadBalancing = "LoadBalanceLoadBased" #Based on Physical Nic Load, change as required (LoadBalanceLoadBased, LoadBalanceIP, LoadBalanceSrcMac, LoadBalanceSrcId, ExplicitFailover)
+$ActiveUP = "Uplink 1" #Modify as required
+$StandUp = "Uplink 2" #Modify as required
+$UnusedUP = "Uplink 3" #Modify as required
 
 ################################################################
 
@@ -18,4 +21,4 @@ Connect-VIServer $vCenter -Credential (Get-Credential) -force
 Get-VDSwitch -Name $vDSName | New-VDPortGroup -Name $VDPG -VLanId $VLAN -NumPorts 8
 
 #Set Load Balancing option
-Get-VDswitch -Name $vDSName | Get-VDPortgroup $VDPG | Get-VDUplinkTeamingPolicy | Set-VDUplinkTeamingPolicy -LoadBalancingPolicy $LoadBalancing
+Get-VDswitch -Name $vDSName | Get-VDPortgroup $VDPG | Get-VDUplinkTeamingPolicy | Set-VDUplinkTeamingPolicy -LoadBalancingPolicy $LoadBalancing -ActiveUplinkPort $ActiveUP -StandbyUplinkPort $StandUp -UnusedUplinkPort $UnusedUP
