8e73f2d132510d6b77a5fc99408ecd33ecda6258
smctighevcp <65562694+smctighevcp@users.noreply.github.com>
2022-01-01 12:20:06 +0000

    Initial commit

---
+++ Functions/APIs/validate-vcenterprofiles.ps1
@@ -0,0 +1,56 @@
+function Validate-vCenterProfiles {
+    <#
+    .SYNOPSIS
+        Validate the vCenter Server Profiles against a target vCenter Server.
+    .DESCRIPTION
+        Validates the vCenter Server Profiles against a target vCenter Server.
+    .PARAMETER vCenterFQDN
+        The vCenter Server FQDN.
+    .PARAMETER SessionID
+        SesionID to authenticate with the vCenter Server.
+    .PARAMETER jsonPath
+        Path to the JSON configuration file to validate.
+    .NOTES
+        Tags: VMware, vCenter, Profiles, PowerCLI, API
+        Author: Stephan McTighe
+        Website: stephanmctighe.com
+    .LINK
+        https://github.com/smctighevcp
+    .EXAMPLE
+        PS C:\> Validate-vCenterProfiles -vCenterFQDN vcsa-02.domain.com -SessionID d84b95e370f1ed68b997f4affbe6feba -jsonPath C:\temp
+        Validates the vCenter Server Profiles from the vCenter server vcsa-01.domain.com against vcsa-02.domain.com.
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline)]
+        [string] $SessionID,
+        [Parameter(Mandatory)]
+        [string] $vCenterFQDN,
+        [Parameter(Mandatory)]
+        [string] $ExportPath
+
+    )
+    Begin {
+        $SessionHeaders = @{
+            "vmware-api-session-id" = "$($SessionID)"
+            "Content-type"          = "application/json"
+        }
+        $body = Convertto-json @{
+            'config_spec' = Get-Content "$($ExportPath)\vcenter-profile-export.json"
+        }
+    }
+    Process {
+        try {
+            $validate = Invoke-RestMethod -Method POST -Uri "https://$($vCenterFQDN)/api/appliance/infraprofile/configs?action=validate&vmw-task=true" -Headers $SessionHeaders -Body $body
+            $validate
+        } catch {
+            Write-Host "An error occurred!" -ForegroundColor Red
+            if ($_.ErrorDetails -like "*Authentication required*") {
+                Write-Host "Possible authentication issue, check username and password ... " -ForegroundColor Blue
+            }
+            Write-Host "Full error ... " -ForegroundColor Blue
+            Write-Host $_.ErrorDetails -ForegroundColor Red
+        }
+    }
+}
